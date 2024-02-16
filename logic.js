0095f68e8cd4f0da90b1874e2496733b38e9142d
Kara Venkateswar Rao <kvenkateswarrao877@gmail.com>
2024-02-16 19:36:17 +0530

    add continuation of project

---
+++ logic.js
@@ -0,0 +1,13 @@
+const filled = document.querySelector(".filled");
+
+
+const massage = () =>{
+   
+        if(filled.required === true){
+            alert("booking successfull")
+        }
+        console.log(filled.required);
+    
+}
+    
+ 
